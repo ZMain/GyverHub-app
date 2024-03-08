ddb4f725ea10e231389f1376b4b2c9ca245281aa
DiTheRX <mrrobinzon2@gmail.com>
2024-03-08 15:05:59 +0300

    Fix Link

--- lib/env.dart
+++ lib/env.dart
@@ -4,8 +4,8 @@
 import 'package:flutter/material.dart';
 
 class Env{
-  static const versionUrl = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/version.txt";
-  static const hubUrl = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/app/index.html";
+  static const versionUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/version.txt";
+  static const hubUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/GyverHub.html";
 
   static const primColor = Color(0xFF37a93c);
   static const bgColorDark = Color(0xFF26272c);
